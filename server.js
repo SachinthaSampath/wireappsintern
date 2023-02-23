2792975c88b77fbf57725cd655ea0b5a59b66c69
Sachintha Sampath <sachinsampath42@gmail.com>
2023-02-24 04:55:45 +0530

    Complete

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
 
-//show static HTML
-app.use(express.static("public"));
-app.use(express.urlencoded());
+const cookieParser = require('cookie-parser')
+app.use(cookieParser());
+const session = require("express-session");
+//set session middleware
+app.use(
+  session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-function logger(req,res,next){
-    console.log(req.originalUrl);
-    next();
-}
-// app.use(logger);
 
-
+//set view engine - to render web pages using templates
 app.set("view engine", "ejs");
 
-app.get("/",logger,logger,logger, (req, res, next) => {
-    console.log("here");
-    // res.download('index.html');
-    // res.status(500).send("Hi");
-    // res.status(500).json({"name":"sachintha"});
-    res.render("index", { text: "world" });
-});
+//set public folder
+app.use(express.static("public"));
 
-const userRouter = require("./routes/users");
-app.use("/users", userRouter);
+//use middleware to handle incomming payloads
+app.use(express.urlencoded());
 
-const customerRouter = require("./routes/customers");
-const exp = require("constants");
-app.use("/customers", customerRouter);
+//set user router
+const userRouter = require("./routes/user");
+app.use("/user", userRouter);
+
+//set customer router
+const customerRouter = require("./routes/customer");
+app.use("/customer", customerRouter);
+
+//set medicine router
+const medicineRouter = require("./routes/medicine");
+app.use("/medicine", medicineRouter);
 
 app.listen(3000);
-
-
